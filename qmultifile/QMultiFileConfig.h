a2cce046841c8c94ada96cb954982f88b0fe31aa
Benjamin Böhmke <benjamin@boehmke.net>
2016-10-17 10:03:58 +0200

    increased version

--- qmultifile/QMultiFileConfig.h
+++ qmultifile/QMultiFileConfig.h
@@ -20,9 +20,9 @@ namespace CFG {
  * @brief Version of this library
  * 1. Byte major  Version -> 0
  * 2. Byte update Version -> 4
- * 3. Byte minor  Version -> 3
+ * 3. Byte minor  Version -> 4
  */
-const quint32 VERSION = 0x000403;
+const quint32 VERSION = 0x000404;
 
 }
 
